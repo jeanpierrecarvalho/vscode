6607350a3505403e5026ddbc2a121fcb856e435a
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-11 19:02:36 +0100

    chore(root): install knip and add configuration

---
+++ knip.config.js
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+const config = {
+	workspaces: {
+		'.': {
+			entry: [],
+			ignore: [],
+			ignoreDependencies: [],
+			ignoreBinaries: [],
+		},
+	},
+};
+
+export default config;
